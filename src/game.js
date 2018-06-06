7ded7ad5638e1e16320d16b2a89b0c943e1026ea
Torinberger <torinberger@torinberger.com>
2018-06-07 08:46:43 +1000

    console css + containers + example command w/jquery

---
+++ src/game.js
@@ -0,0 +1,6 @@
+
+// # Console
+
+$(document).ready(function () {
+  $("#console-text").append("<p class='command'><span class='user'>admin@hack-master</span><span class='white'>$</span> sudo rm -rf /</p>");
+});
